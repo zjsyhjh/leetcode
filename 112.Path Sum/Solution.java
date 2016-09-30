a574960478ee7d3e337f6e0c68e70439d2b796b9
Jiahuan <zjsyhjh@gmail.com>
2016-09-30 19:52:48 +0800

    Path Sum passed

---
+++ 112.Path Sum/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+        	return false;
+        }
+       	if (root.left == null && root.right == null) {
+       		return root.val == sum;
+       	}
+       	if (root.left != null) {
+       		if (hasPathSum(root.left, sum - root.val)) {
+       			return true;
+       		}
+       	}
+       	if (root.right != null) {
+       		if (hasPathSum(root.right, sum - root.val)) {
+       			return true;
+       		}
+       	}
+       	return false;
+    }
+}
