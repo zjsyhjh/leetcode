81e71ac2a6e17a17a066381e763e09031a6fbd70
Jiahuan <zjsyhjh@gmail.com>
2017-03-17 15:10:51 +0800

    problem 81 solved

---
+++ 81.Search in Rotated Sorted Array II/Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }    
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low += 1;
+                high -= 1;
+            } else {
+                if (nums[low] <= nums[mid]) {
+                    if (nums[low] <= target && target < nums[mid]) high = mid - 1;
+                    else low = mid + 1;
+                } else {
+                    if (nums[mid] < target && target <= nums[high]) low = mid + 1;
+                    else high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
